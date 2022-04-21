b2fa9bfb5bd7cb4c556e18a48e1a6d3d102a7ce3
kooleila <leila.hieta@fmi.fi>
2022-04-21 12:46:57 +0300

    cron code

--- kriging_er_correct_elev_stere.R
+++ kriging_er_correct_elev_stere.R
@@ -10,31 +10,48 @@ library('httr')
 library('sp')
 library('fastgrid')
 library('rgeos')
-library('raster')
-library('outliers')
-source('/home/users/hietal/statcal/R_projects/nowcasting_backup/gridding_elev_stere.R') # subroutines
-source('/home/users/hietal/statcal/R_projects/nowcasting_backup/smartmet_obs_elev.R') # obs fetch from smartmet server
-source('file-access.R') # wrapper for reading/writing files from/to s3
-
+#library('raster')
+#library('outliers')
+source('gridding_elev_stere.R') # subroutines
+source('smartmet_obs_elev.R') # obs fetch from smartmet server
+#source('file-access.R') # wrapper for reading/writing files from/to s3
 library('data.table')
 
-args = commandArgs(trailingOnly=TRUE)
+#args = commandArgs(trailingOnly=TRUE)
 #args[1] = input-file
 #args[2] = output-file
 #args[3] = Netatmo TRUE/FALSE
 
-if (length(args) == 0) {
-  quit(status=1)
-}
+#if (length(args) == 0) {
+#  quit(status=1)
+#}
 
-#g_in <- read_s3(args[1])
 #g_out <- strip_protocol(args[2])
 
-use_NetA <- args[3] # TRUE if NetAtmo data is used, FALSE if not
+#use_NetA <- args[3] # TRUE if NetAtmo data is used, FALSE if not
 
-g_in = '/data/hietal/pot.grib2'
+#g_in = '/data/hietal/pot.grib2'
 #g_in <- '/data/hietal/snwc_dev.grib2'
-g_out = 'joku.grib2'
+#g_out = 'joku.grib2'
+
+#the latest origtime used to create POT analysis field (just the metadata is copied)
+#otime_old <- as.character(as.numeric(format(as.POSIXlt((Sys.time()-60*60), tz = "UTC"), "%Y%m%d%H"))) # time in utc -1h
+otime <- as.character(as.numeric(format(as.POSIXlt(Sys.time(), tz = "UTC"), "%Y%m%d%H"))) # current uts in hours
+g_out <- paste('/data/hietal/POT/analysis/',otime,'_0h.grib2',sep='')
+#Ukkosdata 
+txt <- paste("https://routines-data.lake.fmi.fi/smartmet-nwc/development/2.5/",otime,"/fcst_tstm.grib2",sep="")
+destf <- paste('/data/hietal/POT/analysis/',otime,'.grib2',sep='')
+download.file(url=txt,destfile=destf,quiet=TRUE)#, method="",extra = '--no-proxy --no-check-certificate',quiet=TRUE)
+
+# t1<- as.character(as.numeric(format(as.POSIXlt((Sys.time()-0*60), tz = "UTC"), "%Y%m%d%H")))
+# t2<- as.character(as.numeric(format(as.POSIXlt((Sys.time()+60*60), tz = "UTC"), "%Y%m%d%H")))
+# t3<- as.character(as.numeric(format(as.POSIXlt((Sys.time()+120*60), tz = "UTC"), "%Y%m%d%H")))
+# t4<- as.character(as.numeric(format(as.POSIXlt((Sys.time()+180*60), tz = "UTC"), "%Y%m%d%H")))
+# #Current local hour 
+# HOD <- strftime(as.POSIXct(Sys.time()), format="%H")
+# tt <- c(paste("/data/hietal/POT/",HOD,"/",t1,"00.grib2",sep=""),paste("/data/hietal/POT/",HOD,"/",t2,"00.grib2",sep=""),paste("/data/hietal/POT/",HOD,"/",t3,"00.grib2",sep=""),paste("/data/hietal/POT/",HOD,"/",t4,"00.grib2",sep=""))
+# first save the individual tpratet msg-files for motion vector using previous SNWC analysis time 
+g_in <- destf
 
 # data.frame to map grib input-file parameterNumber to corresponding smartmet server OBS name and error weights 
 grib_paramnb <- data.frame('parname'=c('2t','rh','ws','tstm'),'parnumb'=c(0,192,1,2),'obs_parm'=c('TA_PT1M_AVG','RH_PT1M_AVG','WS_PT10M_AVG','flash'),'w1'=c(0.9,0.9,0.8,NaN),'w2'=c(0.9,0.8,0.7,NaN),'w3'=c(0.8,0.7,0.6,NaN),'w4'=c(0.7,0.7,0.6,NaN),stringsAsFactors=FALSE)
@@ -59,17 +76,18 @@ par.row <- which(grib_paramnb$parnumb==param)
 # g_in <- 'smnwc-FF-MS.grib2' # wind speed test dataset
 
 # correlation length km
-clen <- 30 # limited for SYNOP from 50 --> 30km 
-clenx <- 10 # smaller correlation length used for NetAtmo data
+#clen <- 30 # limited for SYNOP from 50 --> 30km 
+#clenx <- 10 # smaller correlation length used for NetAtmo data
+clenf <- 20 # correlation length for flash strikes
 # land sea mask used
-LSM <- readRDS('/home/users/hietal/statcal/R_projects/nowcasting_biascorrection/MEPS_lsm.Rds') # only sea (+Vänern and Wettern)
+#LSM <- readRDS('MEPS_lsm.Rds') # only sea (+Vänern and Wettern)
 #coordnames(LSM) <- c('longitude','latitude') # needed for gridding (should be fixed!)
 # Model elevation
-MELEV <- readRDS('/home/users/hietal/statcal/R_projects/nowcasting_biascorrection/MEPS_alt.Rds')
+#MELEV <- readRDS('MEPS_alt.Rds')
 #coordnames(MELEV) <- c('longitude','latitude')
 
-DEM <- raster("/data/statcal/projects/meps_calibration/data/lcc/HARP_dem_new_meps_100m.tif")
-DEM1 <- raster("/data/statcal/projects/meps_calibration/data/wgs84/HARP_dem_new_100m.tif")
+#DEM <- raster("/data/statcal/projects/meps_calibration/data/lcc/HARP_dem_new_meps_100m.tif")
+#DEM1 <- raster("/data/statcal/projects/meps_calibration/data/wgs84/HARP_dem_new_100m.tif")
 # Forecast leadtimes 15min/1h (depends on the data) steps in Metcoop nowcast
 mtimes <- getfcdates(g_in) # in grib files times <10 with 3 numbers, this function works also when 00:00 --> 0!
 
@@ -103,7 +121,7 @@ if (obs_param == 'flash') {
     obs$observation <- 1
     obs <- obs_spatial(obs)
     obsx <- obs_prepare(obs,raster::extent(out))
-    var.pred <- gridobs(obsx,out,clen=1000*clenx)# lsm=LSM$lsm)
+    var.pred <- gridobs(obsx,out,clen=1000*clenf)# lsm=LSM$lsm)
     # diff = modified - original --> error correction 
     fcerr <- var.pred$diff  # error correction
     #fc.mod <- qcheck(var.pred$VAR1,param,grib_paramnb)
@@ -121,135 +139,138 @@ if (obs_param == 'flash') {
     
     #savegrib(g_in,g_out,msg = m, newdata = fc.mod, append=FALSE)
     savegrib(g_in,g_out,msg = m, newdata = var.pred$VAR1, append=FALSE)
-  } else { # if obs == NULL save just the input fields 
-    savegrib(g_in,g_out,msg = m, newdata = out$VAR1, append=FALSE)
+    } else if (file.exists(destf)) { # if obs == NULL remove downloaded grib-files  
+        file.remove(destf) # delete the original grib file if no thunder
+  #  savegrib(g_in,g_out,msg = m, newdata = out$VAR1, append=FALSE)
   }
   
   
 }
 
-# obs from smartmet server
-# keyword for smartmet server obs retrieval: snwc includes the stations in ErrCorrectStations.csv
-obs <- readobs_all(t1,t1,obs_param,spatial = TRUE)
 
-# if wind speed, then use potential wind speed opervations when available (for Finland)
-if(param==1)  {
-  # retrieve WSP obs data
-  obsPT <- readobs_all(t1,t1,"WSP_PT10M_AVG",spatial = TRUE)
-  # define the indexes for which there's WSP available 
-  ind_fmisid <- which(obs$fmisid %in% obsPT$fmisid)
-  # extra check in case there are WSPT values but no WS values (this has happened for station 101061) 
-  obsPT <- obsPT[which(obsPT$fmisid %in% obs$fmisid),]
-  # replace original obs values with WSP when available
-  obs$observation <- replace(obs$observation,ind_fmisid,obsPT$observation)
-}
 
-if(!is.null(obs)) { # do error correction if there's obs available, if not return input fields 
-  # prepare obs
-  obsx <- obs_prepare(obs,t1,raster::extent(out), LSM, MELEV)
-  # obs for 2t in C --> K in model data
-  # obs for rh 0-100 --> 0-1 in model data
-  if (param==grib_paramnb$parnumb[1]){ # temperature from K --> C
-    obsx$VAR1 <- obsx$VAR1 + 273.15
-  }
-  if (param==grib_paramnb$parnumb[2]){ # RH from 0-1 --> 0-100
-    obsx$VAR1 <- obsx$VAR1/100
-  }
-
-  # gridding
-  var.pred <- gridobs(obsx,out,clen=1000*clen, lsm=LSM$lsm, melev = MELEV$melev)
-  
-  # diff = modified - original --> error correction 
-  fcerr <- var.pred$diff  # error correction
-
-  # quality control:
-  # RH<0-->0 & RH>1-->1
-  # ws<0-->0
-  fc.mod <- qcheck(var.pred$VAR1,param,grib_paramnb)
-  # limit the modifications BC can do
-  # RH diff field max +-20% 
-  var.pred$diff <- diff_qcheck(var.pred$diff,param,grib_paramnb)
-
-  # plot corrected & diff field 
-  var.pred$VAR1 <- fc.mod
-  MOSplotting::MOS_plot_field(var.pred,layer = "VAR1", shapetrans = TRUE,cmin=min(out$VAR1), cmax = max(out$VAR1),
-                                stations = obsx,main=paste(fc_hours[1], 'clen =',clen,'km'),pngfile=paste("mod_var",m,".png",sep=""))
-  
-  MOSplotting::MOS_plot_field(var.pred,layer = "diff", shapetrans = TRUE,cmin=(-5), cmax = 5,
-                              stations = obsx,main=paste(fc_hours[1], 'clen =',clen,'km'),pngfile=paste("SYN_",m,".png",sep=""))
-  
-  # if temperature then add NetAtmo QC corrected obs to bias correction
-  # gridding point observations to background grid using 'fastgrid' package
-  gridobs1 <- function(obs,grid, variable="VAR1", sigma = 5.0, nugget=0.2, clen=1000*10, lsm=NULL) {
-    
-    covpars <- c(sigma,clen,nugget)
-    trend_model <- as.formula(paste(variable,'~ -1'))
-    coordnames(grid) <- c('longitude','latitude')
-    var.pred <- fastkriege(trend_model = trend_model, obs, grid, cov.pars = covpars, 
-                           lsm=lsm,lsmy=obs$lsm, alt=NULL, alty=NULL,
-                           bg=grid, variable=variable, LapseRate = 0.0,
-                           method='nearest')
-    
-    return(var.pred)
-  }
-  
-  if (use_NetA == TRUE & param==grib_paramnb$parnumb[1]){ 
-    var.pred$VAR1 <- fc.mod 
-    alku <- t1-(10*60) # NetAtmo data is taken from 10 min time interval xx:50-t1
-    obsNetA <- readobs_all(alku,t1,parname = 'NetAtmo',spatial = TRUE) # fetch NetAtm QC corrected obs
-    if(!is.null(obsNetA)) { # do error correction if there's NetAtmo obs available, if not return SYNOP corrected fields  
-      names(obsNetA)<-c('name','fmisid','time','elevation','observation')
-      obsNetA$observation <- obsNetA$observation + 273.15
-      # prepare obs
-      obsNetAx <- obs_prepareNetA(obsNetA,t1,raster::extent(out),LSM,grid=var.pred,melev=NULL)
-      # interpolate NetAtmo elevation from DEM 100m
-      obsNetAx$elevation <- extract(DEM,obsNetAx)
-      #tes <- extract(DEM1,obsNetAx)
-      # coordnames(obsNetAx) <- c('longitude','latitude')
-      var.predX <- gridobs(obsNetAx,var.pred,clen=1000*clenx,lsm=LSM$lsm, melev = MELEV$melev)
-      a <- Sys.time()
-      var.predX1 <- gridobs1(obsNetAx,var.pred,clen=1000*clenx,lsm=LSM$lsm)
-      b <- Sys.time()-a
-      # diff = modified - original --> error correction 
-      # error correction "fcerr" is now the SYNOP+NETATMO corrected field minus original model data   
-      fcerr <- var.predX$VAR1 - out$VAR1 # var.predX$diff  # error correction
-      fc.mod <- qcheck(var.predX$VAR1,param,grib_paramnb)
-      
-  MOSplotting::MOS_plot_field(var.predX,layer = "diff", shapetrans = TRUE,cmin=(-5), cmax = 5,
-                          main=paste(fc_hours[1], 'clen =',clenx,'km'),zoom=c(-1000,300000,8800000,9200000),pngfile=paste("mod_NA",m,".png",sep=""))
-  MOSplotting::MOS_plot_field(var.predX1,layer = "diff", shapetrans = TRUE,cmin=(-5), cmax = 5,
-                              main=paste(fc_hours[1], 'clen =',clenx,'km'),zoom=c(-1000,300000,8800000,9200000),pngfile=paste("mod_NA",m,".png",sep=""))
-  # MOSplotting::MOS_plot_field(var.predX,layer = "VAR1", shapetrans = TRUE,cmin=(250), cmax = 290,
-  #                            stations = obsNetAx,main=paste(fc_hours[1], 'clen =',clen,'km'),pngfile=paste("mod_VAR_NA",m,".png",sep=""))
-  # MOSplotting::MOS_plot_field(var.pred,layer = "VAR1", shapetrans = TRUE,cmin=(250), cmax = 290,
-  #                            main=paste(fc_hours[1], 'clen =',clen,'km'),pngfile=paste("mod_",m,".png",sep=""))
-  # MOSplotting::MOS_plot_field(out,layer = "VAR1", shapetrans = TRUE,cmin=(250), cmax = 290,
-  #                                main=paste(fc_hours[1], 'clen =',clen,'km'),pngfile=paste("mod_",m,".png",sep=""))
-      
-    }
-  }
-  savegrib(g_in,g_out,msg = m, newdata = fc.mod, append=FALSE)
-
-  # now add the 1h correction to 2h,3h,4h and 5h
-  wcorr <- as.vector(t(grib_paramnb[par.row,c('w1','w2','w3','w4')])) # choose the relevant error weights 
-  for (m in 1:(length(msgs)-1)) {
-    mm <- msgs[2:length(msgs)][m] # index of grib message
-    out2 <- readgrib(g_in,msg=mm) # raw model data 
-    tmp_fc.mod2 <- out2$VAR1 + wcorr[m] * fcerr # error correction using varying weights for different forecast times
-    fc.mod2 <- qcheck(tmp_fc.mod2,param,grib_paramnb)
-    savegrib(g_in,g_out,msg = mm, newdata = fc.mod2, append=TRUE)
-  }
-} else { # if obs == NULL save just the input fields 
-  for (m in 1:(length(msgs))) {
-    mm <- msgs[m] # index of grib message
-    out2 <- readgrib(g_in,msg=mm) # raw model data 
-    fc.mod2 <- out2$VAR1
-    #fc.mod2 <- qcheck(tmp_fc.mod2,param,grib_paramnb)
-    savegrib(g_in,g_out,msg = mm, newdata = fc.mod2, append=TRUE)
-  }
-}
-
-write_s3(args[2])
+# # obs from smartmet server
+# # keyword for smartmet server obs retrieval: snwc includes the stations in ErrCorrectStations.csv
+# obs <- readobs_all(t1,t1,obs_param,spatial = TRUE)
+# 
+# # if wind speed, then use potential wind speed opervations when available (for Finland)
+# if(param==1)  {
+#   # retrieve WSP obs data
+#   obsPT <- readobs_all(t1,t1,"WSP_PT10M_AVG",spatial = TRUE)
+#   # define the indexes for which there's WSP available 
+#   ind_fmisid <- which(obs$fmisid %in% obsPT$fmisid)
+#   # extra check in case there are WSPT values but no WS values (this has happened for station 101061) 
+#   obsPT <- obsPT[which(obsPT$fmisid %in% obs$fmisid),]
+#   # replace original obs values with WSP when available
+#   obs$observation <- replace(obs$observation,ind_fmisid,obsPT$observation)
+# }
+# 
+# if(!is.null(obs)) { # do error correction if there's obs available, if not return input fields 
+#   # prepare obs
+#   obsx <- obs_prepare(obs,t1,raster::extent(out), LSM, MELEV)
+#   # obs for 2t in C --> K in model data
+#   # obs for rh 0-100 --> 0-1 in model data
+#   if (param==grib_paramnb$parnumb[1]){ # temperature from K --> C
+#     obsx$VAR1 <- obsx$VAR1 + 273.15
+#   }
+#   if (param==grib_paramnb$parnumb[2]){ # RH from 0-1 --> 0-100
+#     obsx$VAR1 <- obsx$VAR1/100
+#   }
+# 
+#   # gridding
+#   var.pred <- gridobs(obsx,out,clen=1000*clen, lsm=LSM$lsm, melev = MELEV$melev)
+#   
+#   # diff = modified - original --> error correction 
+#   fcerr <- var.pred$diff  # error correction
+# 
+#   # quality control:
+#   # RH<0-->0 & RH>1-->1
+#   # ws<0-->0
+#   fc.mod <- qcheck(var.pred$VAR1,param,grib_paramnb)
+#   # limit the modifications BC can do
+#   # RH diff field max +-20% 
+#   var.pred$diff <- diff_qcheck(var.pred$diff,param,grib_paramnb)
+# 
+#   # plot corrected & diff field 
+#   var.pred$VAR1 <- fc.mod
+#   MOSplotting::MOS_plot_field(var.pred,layer = "VAR1", shapetrans = TRUE,cmin=min(out$VAR1), cmax = max(out$VAR1),
+#                                 stations = obsx,main=paste(fc_hours[1], 'clen =',clen,'km'),pngfile=paste("mod_var",m,".png",sep=""))
+#   
+#   MOSplotting::MOS_plot_field(var.pred,layer = "diff", shapetrans = TRUE,cmin=(-5), cmax = 5,
+#                               stations = obsx,main=paste(fc_hours[1], 'clen =',clen,'km'),pngfile=paste("SYN_",m,".png",sep=""))
+#   
+#   # if temperature then add NetAtmo QC corrected obs to bias correction
+#   # gridding point observations to background grid using 'fastgrid' package
+#   gridobs1 <- function(obs,grid, variable="VAR1", sigma = 5.0, nugget=0.2, clen=1000*10, lsm=NULL) {
+#     
+#     covpars <- c(sigma,clen,nugget)
+#     trend_model <- as.formula(paste(variable,'~ -1'))
+#     coordnames(grid) <- c('longitude','latitude')
+#     var.pred <- fastkriege(trend_model = trend_model, obs, grid, cov.pars = covpars, 
+#                            lsm=lsm,lsmy=obs$lsm, alt=NULL, alty=NULL,
+#                            bg=grid, variable=variable, LapseRate = 0.0,
+#                            method='nearest')
+#     
+#     return(var.pred)
+#   }
+#   
+#   if (use_NetA == TRUE & param==grib_paramnb$parnumb[1]){ 
+#     var.pred$VAR1 <- fc.mod 
+#     alku <- t1-(10*60) # NetAtmo data is taken from 10 min time interval xx:50-t1
+#     obsNetA <- readobs_all(alku,t1,parname = 'NetAtmo',spatial = TRUE) # fetch NetAtm QC corrected obs
+#     if(!is.null(obsNetA)) { # do error correction if there's NetAtmo obs available, if not return SYNOP corrected fields  
+#       names(obsNetA)<-c('name','fmisid','time','elevation','observation')
+#       obsNetA$observation <- obsNetA$observation + 273.15
+#       # prepare obs
+#       obsNetAx <- obs_prepareNetA(obsNetA,t1,raster::extent(out),LSM,grid=var.pred,melev=NULL)
+#       # interpolate NetAtmo elevation from DEM 100m
+#       obsNetAx$elevation <- extract(DEM,obsNetAx)
+#       #tes <- extract(DEM1,obsNetAx)
+#       # coordnames(obsNetAx) <- c('longitude','latitude')
+#       var.predX <- gridobs(obsNetAx,var.pred,clen=1000*clenx,lsm=LSM$lsm, melev = MELEV$melev)
+#       a <- Sys.time()
+#       var.predX1 <- gridobs1(obsNetAx,var.pred,clen=1000*clenx,lsm=LSM$lsm)
+#       b <- Sys.time()-a
+#       # diff = modified - original --> error correction 
+#       # error correction "fcerr" is now the SYNOP+NETATMO corrected field minus original model data   
+#       fcerr <- var.predX$VAR1 - out$VAR1 # var.predX$diff  # error correction
+#       fc.mod <- qcheck(var.predX$VAR1,param,grib_paramnb)
+#       
+#   MOSplotting::MOS_plot_field(var.predX,layer = "diff", shapetrans = TRUE,cmin=(-5), cmax = 5,
+#                           main=paste(fc_hours[1], 'clen =',clenx,'km'),zoom=c(-1000,300000,8800000,9200000),pngfile=paste("mod_NA",m,".png",sep=""))
+#   MOSplotting::MOS_plot_field(var.predX1,layer = "diff", shapetrans = TRUE,cmin=(-5), cmax = 5,
+#                               main=paste(fc_hours[1], 'clen =',clenx,'km'),zoom=c(-1000,300000,8800000,9200000),pngfile=paste("mod_NA",m,".png",sep=""))
+#   # MOSplotting::MOS_plot_field(var.predX,layer = "VAR1", shapetrans = TRUE,cmin=(250), cmax = 290,
+#   #                            stations = obsNetAx,main=paste(fc_hours[1], 'clen =',clen,'km'),pngfile=paste("mod_VAR_NA",m,".png",sep=""))
+#   # MOSplotting::MOS_plot_field(var.pred,layer = "VAR1", shapetrans = TRUE,cmin=(250), cmax = 290,
+#   #                            main=paste(fc_hours[1], 'clen =',clen,'km'),pngfile=paste("mod_",m,".png",sep=""))
+#   # MOSplotting::MOS_plot_field(out,layer = "VAR1", shapetrans = TRUE,cmin=(250), cmax = 290,
+#   #                                main=paste(fc_hours[1], 'clen =',clen,'km'),pngfile=paste("mod_",m,".png",sep=""))
+#       
+#     }
+#   }
+#   savegrib(g_in,g_out,msg = m, newdata = fc.mod, append=FALSE)
+# 
+#   # now add the 1h correction to 2h,3h,4h and 5h
+#   wcorr <- as.vector(t(grib_paramnb[par.row,c('w1','w2','w3','w4')])) # choose the relevant error weights 
+#   for (m in 1:(length(msgs)-1)) {
+#     mm <- msgs[2:length(msgs)][m] # index of grib message
+#     out2 <- readgrib(g_in,msg=mm) # raw model data 
+#     tmp_fc.mod2 <- out2$VAR1 + wcorr[m] * fcerr # error correction using varying weights for different forecast times
+#     fc.mod2 <- qcheck(tmp_fc.mod2,param,grib_paramnb)
+#     savegrib(g_in,g_out,msg = mm, newdata = fc.mod2, append=TRUE)
+#   }
+# } else { # if obs == NULL save just the input fields 
+#   for (m in 1:(length(msgs))) {
+#     mm <- msgs[m] # index of grib message
+#     out2 <- readgrib(g_in,msg=mm) # raw model data 
+#     fc.mod2 <- out2$VAR1
+#     #fc.mod2 <- qcheck(tmp_fc.mod2,param,grib_paramnb)
+#     savegrib(g_in,g_out,msg = mm, newdata = fc.mod2, append=TRUE)
+#   }
+# }
+# 
+# write_s3(args[2])
 
 
 # extra plotting
---
+++ smartmet_obs_elev.R
@@ -0,0 +1,140 @@
+# fetch obs from smartmet server
+# Station_list used is keyword=snwc
+# The first guess station_list is MOS station_list, only SYNOP stations
+
+library('httr')
+
+# Do conversion to spatial object if server returns numbers, if not then return NULL  
+obs_spatial <- function(obs) {
+  tryCatch(
+    {
+    obs <- obs[complete.cases(obs$latitude),]
+    obs <- obs[obs$latitude>-90,] # exclude south pole!
+    sp::coordinates(obs) <- c('longitude','latitude')
+    sp::proj4string(obs) <- sp::CRS("+init=epsg:4326")
+    return(obs)
+    },
+    # how to handle warnings 
+    warning = function(cond) {
+      message(cond)
+      obs = NULL
+      return(obs)
+    },
+    # how to handle errors
+    error = function(cond) {
+      message(cond)
+      obs = NULL
+      return(obs)
+    }
+  )
+}
+
+# Read observations from smartmet server
+readobs <- function(starttime, endtime, parname,
+  parameters = c('name','fmisid','latitude','longitude','time','elevation'),
+  query = list(),
+  spatial=FALSE) {
+
+  url <- 'http://smartmet.fmi.fi/timeseries'
+  sep <- I(';') # csv separator I()
+  
+  # default query parameters
+  defquery = list(    
+    format='ascii',
+    separator=sep,
+    timeformat='sql',
+    tz='gmt',
+    starttime=format(as.POSIXct(starttime),'%Y%m%d%H%M'),
+    endtime=format(as.POSIXct(endtime),'%Y%m%d%H%M'),
+    keyword='snwc',
+    timestep='1h',
+    precision='full',
+    #data_quality='1',
+    #param=paste(parameters,parname,collapse=I(','),sep=''),
+    param=paste(c(parameters,parname),collapse=I(','),sep=''),
+    producer='observations_fmi'    
+  )
+
+  # modify default query from command line arguments
+  query <- modifyList(defquery,query)
+  outfile <- tempfile(fileext = ".csv")
+  
+  # if no connection to server (times out) then return NULL
+  tryCatch(
+    {
+    httr::GET(url=url, query=query, httr::write_disk(outfile,overwrite = TRUE),timeout(20))
+    },
+    # how to handle warnings 
+    warning = function(cond) {
+      message(cond)
+    },
+    # how to handle errors
+    error = function(cond) {
+    message(cond)
+    }
+  )
+  
+  obs <- read.csv2(outfile,sep=sep,header=FALSE,col.names=c(parameters,'observation'),dec='.',
+                   stringsAsFactors=FALSE)
+  if (nrow(obs)>0) {
+    obs$time <- as.POSIXct(obs$time,tz='UTC')
+    # add QC for obs: based on static thresholds and on distribution (chi-sq test)
+    if (parname=='NetAtmo' | parname=='TA_PT1M_AVG') { # for temperature [-40,40]
+      obs <- obs[(obs$observation>=(-40) & obs$observation<=40),]
+      outliers <- scores(obs$observation, type="chisq", prob=0.995) # chi-sq scores => (x - mean(x))^2/var(x)
+      obs <- obs[outliers==FALSE,]
+    }
+    if (parname=='WS_PT10M_AVG' | parname=='WSP_PT10M_AVG') { # for wind speed [0,40]
+      obs <- obs[(obs$observation>=0 & obs$observation<=40),]
+      outliers <- scores(obs$observation, type="chisq", prob=0.9999) 
+      obs <- obs[outliers==FALSE,]
+    } 
+    if (parname=='RH_PT1M_AVG') { # for relative humidity [0,100]
+      obs <- obs[(obs$observation>=0 & obs$observation<=100),]
+      outliers <- scores(obs$observation, type="chisq", prob=0.9999) 
+      obs <- obs[outliers==FALSE,]
+    } 
+    # convert to SpatialPointsDataFrame
+    if (spatial) {
+      obs <- obs_spatial(obs)
+      # obs <- obs[complete.cases(obs$latitude),]
+      # obs <- obs[obs$latitude>-90,] # exclude south pole!
+      # sp::coordinates(obs) <- c('longitude','latitude')
+      # sp::proj4string(obs) <- sp::CRS("+init=epsg:4326")
+    }
+  
+    file.remove(outfile)
+    return(obs)
+  }
+}
+
+# return Finnish and foreign stations (road weather stations)
+readobs_all <- function(starttime, endtime=NULL, parname,
+                        parameters = c('name','fmisid','latitude','longitude','time','elevation'),
+                        spatial=FALSE) {
+  if (is.null(endtime))
+    endtime <- starttime
+  if (parname!='NetAtmo'){ # if parameter is not NetAtmo data
+    obs1 <- readobs(starttime, endtime, parname, parameters = parameters, spatial = spatial)
+    obs2 <- readobs(starttime, endtime, parname,
+                   query=list(producer='foreign'),
+                   parameters = parameters, spatial = spatial)
+    #obs3 <- readobs(starttime, endtime, fmisid, parname,  # road weather station
+    #                query=list(producer='road'),
+    #                parameters = parameters, spatial = spatial)
+    #tmp_list <- list(obs1,obs2,obs3)
+    tmp_list <- list(obs1,obs2)
+    tmp_list <- tmp_list[lengths(tmp_list) != 0]
+    #joku = tmp_list[-which(sapply(tmp_list, is.null))]
+    #tmp_list=tmp_list[-(which(sapply(tmp_list,is.null),arr.ind=TRUE))] #remove NULL
+    obs <- do.call("rbind", tmp_list)
+    #obs <- obs[complete.cases(obs@data$TA_PT1M_AVG),] # Remove if obs for T is Nan!!!
+  } else if (parname=='NetAtmo') {
+    #obs <- fread(paste("http://smartmet.fmi.fi/timeseries?producer=NetAtmo&tz=gmt&precision=full&starttime=",alku1,"&endtime=",loppu1,"&param=data,station_id,longitude,latitude,utctime,temperature&format=ascii&data_quality=1&bbox=7,55,32,70",sep=""))
+    obs <- readobs(starttime, endtime, parname='temperature',
+                    query=list(producer='NetAtmo',data_quality='1'), 
+                    parameters = c('name','station_id','latitude','longitude','time','altitude'), spatial = spatial)
+  }
+    
+  return(obs)
+}
